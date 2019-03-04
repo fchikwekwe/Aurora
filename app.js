7d10c8c274dc73cd978094fd27d42208b4fe49f1
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-03-04 14:06:07 -0800

    ⚡️ additional set-up stuff; environment variables

---
+++ app.js
@@ -0,0 +1,64 @@
+/*
+ *  FaceSpace Main Server
+ */
+
+/** Require environment variable(s) */
+require('dotenv').config();
+
+/** Require middlewares */
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const config = require('./config/config');
+const app = require('./config/express');
+const routes = require('./index.route');
+
+/** Instantiate server */
+const PORT = process.env.PORT || 3000;
+
+/** Use middlewares */
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static('./carfew-react/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(routes);
+
+/** Custom auth-checking middleware */
+const checkAuth = (req, res, next) => {
+    if (typeof req.cookies.faceToken === 'undefined' || req.cookies === null) {
+        req.user = null;
+    } else {
+        const token = req.cookies.faceToken;
+        const decodedToken = jwt.decode(token, { complete: true }) || {};
+        req.user = decodedToken.payload;
+    }
+    next();
+};
+
+app.use(checkAuth);
+
+/** Database connection */
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/facespace', {
+    useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected successfully.');
+});
+
+/** Require controller(s) */
+
+/** Port listener */
+app.listen(PORT, () => {
+    console.log('FaceSpace listening on port', PORT);
+});
+
+module.exports = app;

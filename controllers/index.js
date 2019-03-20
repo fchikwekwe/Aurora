6c342d9c02b989b2972340e186df557b8e2e88f1
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-03-20 12:07:56 -0700

    ✅ working out a few bugs with new API

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,19 @@
 /** Root route and API routes go here */
 
+require('@tensorflow/tfjs-node');
+
+const faceapi = require('face-api.js');
+const canvas  = require('canvas');
+// const fetch = require('node-fetch');
+// const path = require('path');
+
+const { Canvas, Image, ImageData } = canvas;
+
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+// faceapi.env.monkeyPatch({ fetch: fetch });
+
+const MODELS_URL = '/Users/faith1/code/courses/FaceSpace/public/models/';
+
 module.exports = (app) => {
     // ROOT
     app.get('/', (req, res) => {
@@ -9,5 +23,21 @@ module.exports = (app) => {
     // Camera route that renders central app
     app.get('/camera', (req, res) => {
         res.render('camera');
+    });
+
+    // Camera route that renders central app
+    app.get('/example', async (req, res) => {
+        const net = new faceapi.TinyFaceDetector();
+        // console.log(faceapi.nets);
+        await net.loadFromDisk(MODELS_URL);
+        
+        const input = 'inputVideo';
+        const detection = await faceapi.detectSingleFace(input);
+        console.log("DETECTION", detection);
+
+        const result = detection.withFaceLandmarks().withFaceDescriptors();
+
+        console.log("RESULT", result);
+        res.render('webcam');
     });
 };
